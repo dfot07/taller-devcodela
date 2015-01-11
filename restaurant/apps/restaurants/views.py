beab6c008f3ed93bb3f61ff2da4e4ae07e2e7e06
Diego <df_ot@hotmail.com>
2015-01-11 16:38:39 -0500

    Capitulo 4

--- restaurant/apps/restaurants/views.py
+++ restaurant/apps/restaurants/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Category, Payment, City, Restaurant
+
 # Create your views here.
 
 class IndexView(TemplateView):
 	template_name = 'index.html'
+
+	def get_context_data(self, **kargs):
+		context = super(IndexView, self).get_context_data(**kargs)
+		context['categories'] = Category.objects.all()
+		context['payments'] = Payment.objects.all()
+		context['cities'] = City.objects.all()
+		restaurants = Restaurant.objects.all()[:5]
+		tips = [ restaurant.tip_set.all().count() for restaurant in restaurants]
+		context['restaurants'] = zip(restaurants, tips)
+		return context
